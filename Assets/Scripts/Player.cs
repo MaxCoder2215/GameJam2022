4493e890ebe30dbd596f839ccaddfc65923cc7e6
MaxSchaad <kenneth.schaad@blackburn.edu>
2022-01-29 15:24:49 -0600

    Pushing of Script Templates
    Pushing of scripts:
    CharacterDisplay
    CharacterType
    Entity
    Item
    Player
    Stats

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity
+{
+    // The players chosen character, including any stats and items exclusive to the character. Can be traded. 
+    public  CharacterType character_type;
+
+    public Item[] Iventory; 
+
+
+
+
+}
