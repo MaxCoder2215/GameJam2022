21c75c54d65cf43cdc5884e741519042e478701e
MaxSchaad <kenneth.schaad@blackburn.edu>
2022-01-29 16:20:10 -0600

    Item Script Update
    Item Script Update

--- Assets/Scripts/CharacterDisplay.cs
+++ Assets/Scripts/CharacterDisplay.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CharacterDisplay : MonoBehaviour
 {
+    public Player player; 
+
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -2,21 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Entity : MonoBehaviour
+public class Entity 
 {
-    private Stats BaseStats;
-    private Stats CurrentStats;
-    
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Entity(Stats startingStats)
+
+    public Stats base_stats;
+    public Stats current_stats;
+
+
+
 }
