7d8ff365d32b113023ffff5cd28a119fa484de61
Michael Hoff <46758667+MikeTheAnomaly@users.noreply.github.com>
2022-01-30 00:49:33 -0600

    lots of things

--- ServerFiles/GameJam2022Server/Program.cs
+++ ServerFiles/GameJam2022Server/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using Microsoft.Data.Sqlite;
 using Nancy.Hosting.Self;
 static class Program
 {
@@ -6,6 +7,7 @@ static class Program
 
     static void Main()
     {
+        GivePlayerItem(1, "mimicLoses");
         TCPServer.Start();
         using (var host = new NancyHost(new Uri("http://localhost:80")))
         {
@@ -26,6 +28,29 @@ static class Program
 
     public static void GivePlayerItem(int player, string itemID)
     {
-        TCPServer.Messages.Enqueue($"Hello player {player} requests item {itemID}"); ;
+        string outMessage = "";
+        using (var connection = new SqliteConnection("Data Source=C:/Users/michael.hoff/Documents/unity Projects/GameJam2022/ServerFiles/GameJam2022Server/GameJam2022.db"))
+        {
+            connection.Open();
+            Console.WriteLine(connection.Database);
+            var command = connection.CreateCommand();
+            command.CommandText =
+            @"
+        SELECT *
+        FROM Events
+        Where ID = $id;
+    ";
+            command.Parameters.AddWithValue("$id", itemID);
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    outMessage += reader.GetString(0);
+                }
+            }
+        }
+        Console.WriteLine(outMessage);
+        TCPServer.Messages.Enqueue($"out: {outMessage}");
     }
+
 }
