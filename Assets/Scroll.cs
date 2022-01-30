dc31f81a6a3c6a345aca6375c83c9bb222d44ccd
Michael Hoff <46758667+MikeTheAnomaly@users.noreply.github.com>
2022-01-30 16:11:33 -0600

    yeet

--- Assets/Scroll.cs
+++ Assets/Scroll.cs
@@ -6,14 +6,21 @@ public class Scroll : MonoBehaviour
 {
     //https://www.youtube.com/watch?v=HrDxnMI7pCc
     // Start is called before the first frame update
+
+    public float speed = 5f;
+    
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector2 offset = new Vector2 (Time.time * speed, 0);
+
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+
+
     }
 }
